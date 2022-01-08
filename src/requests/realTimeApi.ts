3be1587609618c498958fdf2ee45d9100c7d561d
zigsong <wldms5764@gmail.com>
2022-01-08 17:31:28 +0900

    feat: construct realTimeApi

---
+++ src/requests/realTimeApi.ts
@@ -0,0 +1,56 @@
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { child, getDatabase, onValue, push, ref, update } from 'firebase/database';
+import 'firebase/auth';
+
+const databaseURL = 'https://zipkok-52711-default-rtdb.asia-southeast1.firebasedatabase.app';
+
+export class RealTimeApi {
+  #firebase: FirebaseApp;
+
+  constructor() {
+    this.#firebase = initializeApp({
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+      appId: process.env.REACT_APP_APP_ID,
+      measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    });
+  }
+
+  public fetch<T>(path: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const db = getDatabase();
+      const reference = ref(db, path);
+
+      onValue(reference, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          resolve(data);
+        } else {
+          reject(new Error('firebase get error'));
+        }
+      });
+    });
+  }
+
+  public post<T>(path: string, postBody: T): Promise<void> {
+    return new Promise<void>((_, reject) => {
+      const db = getDatabase();
+      const newPostKey = push(child(ref(db), path)).key;
+
+      try {
+        const updates: Record<string, T> = {};
+        updates[path + newPostKey] = postBody;
+
+        return update(ref(db), updates);
+      } catch (error) {
+        reject('firebase post error: ' + error);
+      }
+    });
+  }
+}
+
+export default new RealTimeApi();
